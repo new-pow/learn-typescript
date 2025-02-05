697bc61cc7745e11686982cc7412652c73d18062
Lee Saehim <103120173+new-pow@users.noreply.github.com>
2025-02-05 21:45:21 +0900

    study: session4

---
+++ getting-started/index.js
@@ -0,0 +1,7 @@
+// ts 확장자
+// js를 구현하여도 되지만, 무의미
+// compile을 통해 js 로 변환하여 사용함
+function minus(a, b) {
+    return a - b;
+}
+minus(10, 20); // 10 - 20 = -10
---
+++ getting-started/index.ts
@@ -0,0 +1,9 @@
+// ts 확장자
+// js를 구현하여도 되지만, 무의미
+// compile을 통해 js 로 변환하여 사용함
+
+function minus(a: number, b: number): number {
+  return a - b;
+}
+
+minus(10, 20); // 10 - 20 = -10
